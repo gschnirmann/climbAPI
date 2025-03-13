1ca708a25e59b1f603a7d60f71ab249f6ec38f77
gschnirmann <gschnirmann@gmail.com>
2025-03-13 09:31:09 -0300

    login com google funcionando

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask import Flask, request, jsonify, render_template, redirect, url_for, session, json
 import sqlite3
 import psycopg2
 import psycopg2.extras
 import os
-
+from authlib.integrations.flask_client import OAuth
 
 
 #seria /user/123/routes
@@ -11,29 +11,117 @@ import os
 #sempre /recurso/id/subrecurso
 
 
+#
+
+
 #Creates a flask class instance
 app = Flask(__name__)
 
+# Variável global para indicar que o app foi reiniciado
+app.config["SESSION_CLEARED_ON_START"] = False  
+
+appConf = {
+    "OAUTH2_META_URL": "https://accounts.google.com/.well-known/openid-configuration",
+    "FLASK_PORT": 5000
+}
+
+
+
+oauth = OAuth(app)
+
+
+#Settings for security. It depends if its running on the deply server or in the localhost for testing. The variables are taken from os.getenv (DB and Google)
+#Its needed a flask secret to create a session.
+FLASK_SECRET = os.getenv("FLASK_SECRET")
+
+if not FLASK_SECRET:
+    app.secret_key = "b1b8060b-162a-42e7-b453-bf546f53c526"
+    google = oauth.register(name='google',
+               client_id = "998972561848-48bcvrgestgv33gaqjsuhk4p08f2oh7f.apps.googleusercontent.com",
+               client_secret="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",
+               authorize_params={"prompt": "select_account"},
+               server_metadata_url=appConf.get("OAUTH2_META_URL"),
+               client_kwargs={
+                   "scope": "openid profile email",
+               })
+else:
+    app.secret_key = FLASK_SECRET
+    google = oauth.register(name='google',
+               client_id = GOOGLE_CLIENT_ID,
+               client_secret = GOOGLE_CLIENT_SECRET,
+               authorize_params={"prompt": "select_account"},
+               server_metadata_url=appConf.get("OAUTH2_META_URL"),
+               client_kwargs={
+                   "scope": "openid profile email",
+               })
+
+
+
+
 port = int(os.environ.get("PORT", 5000))
 
-DATABASE_URL = os.getenv("DATABASE_URL")
 
 def db_connection():
-    """conn = psycopg2.connect(
-        dbname = "climb_api_db",
-        user = "gschnirmann",
-        password = "geleia1924!",
-        host = "localhost",
-        port = "5432"
-    )"""
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    if DATABASE_URL:
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    else:
+        conn = psycopg2.connect(
+            dbname = "climb_api_db",
+            user = "gschnirmann",
+            password = "geleia1924!",
+            host = "localhost",
+            port = "5432"
+        )
+    #vem da variável de ambiente do banco na nuvem.
+    
     return conn
 
+
+@app.before_request
+def force_clear_session():
+    if not app.config["SESSION_CLEARED_ON_START"]:  # Só limpa na primeira request após restart
+        session.clear()
+        app.config["SESSION_CLEARED_ON_START"] = True  # Marca que já limpou uma vez
+
+
+#login for google
+@app.route("/login/google")
+def login_google():
+    try:
+        redirect_uri = url_for('authorize_google', _external=True)
+        return google.authorize_redirect(redirect_uri)
+    except Exception as e:
+        app.logger.error(f"Error during login {str(e)}")
+        return "Error during login", 500
+
+#authorize (callback) for google
+@app.route("/authorize/google")
+def authorize_google():
+    #como armazenar o token no FE?
+    token = google.authorize_access_token()
+    userinfo_endpoint = google.server_metadata['userinfo_endpoint']
+    resp = google.get(userinfo_endpoint)
+    user_info = resp.json()
+    username = user_info['email']
+    name = user_info['name']
+
+    session["username"] = username
+    session['oauth_token'] = token
+    session['name'] = name
+
+    return redirect(url_for("index"))
+
+@app.route("/logout/google", methods=['POST'])
+def logout_google():
+    session.clear()
+    return redirect(url_for("index"))
 
 #main page endpoint
 @app.route('/')
 def index():
-    return render_template('index.html') #renderiza index.html
+    #return render_template('home.html', session=session.get("username"), pretty =json.dumps(session.get("oauth_token"), indent=4))                      
+    return render_template('index.html', session=session.get("username"), pretty =json.dumps(session.get("username"), indent=4)) #renderiza index.html
 
 #endpoint for rendering a login success page
 @app.route('/success')
@@ -56,25 +144,52 @@ def show_login_page():
 #endpoint for searching an user in the database and rendering the main_menu_page
 @app.route('/check_login', methods=['POST'])
 def check_login():
-    user_id = request.form.get('user_id')
-    username = request.form.get('username')
+    #check if there is a google login and if this user is already in the db
+    if session:
+        conn = db_connection()
+        cursor = conn.cursor()
+        #verify if the useranme is already in the db
+        cursor.execute("SELECT user_id FROM users WHERE username = %s", (session["username"],))
+        result = cursor.fetchone()
+        #if so, goes to the menu_page
+        if result:
+            conn.commit()
+            conn.close()
+            return render_template('menu_page.html', username=session["username"], user_id=result[0])
+        #register the new user to the db
+        else:
+            cursor.execute("INSERT INTO users (username) VALUES (%s)", (session["username"],))
+            conn.commit()
 
-    #conn = sqlite3.connect('climb_API_db')
-    conn = db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT user_id FROM users WHERE user_id = %s AND username = %s", (user_id, username))
-    result = cursor.fetchone() #first result or None
-
-    conn.commit()
-    conn.close()
-    
-    if result:
-        #entender se faz sentido passar o username no query parameter ou se faço a busca no bd depois
-        #return redirect(url_for('user_page', user_id=user_id))
-        #return redirect(url_for('list_sent_routes', user_id=user_id)) usei para listar as vias do usuario
-        return render_template('menu_page.html', username=username, user_id=user_id)
+            #get the new user id
+            cursor.execute("SELECT user_id FROM users where username = %s", (session["username"],))
+            result = cursor.fetchone()
+           
+            conn.close()
+            return render_template('menu_page.html', username=session["username"], user_id=result[0])
+        
+    #regular login
     else:
-        return "Error: User not found...", 401
+        user_id = request.form.get('user_id')
+        username = request.form.get('username')
+
+        #conn = sqlite3.connect('climb_API_db')
+        conn = db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT user_id FROM users WHERE user_id = %s AND username = %s", (user_id, username))
+        result = cursor.fetchone() #first result or None
+
+        conn.commit()
+        conn.close()
+        session["username"] = username
+        
+        if result:
+            #entender se faz sentido passar o username no query parameter ou se faço a busca no bd depois
+            #return redirect(url_for('user_page', user_id=user_id))
+            #return redirect(url_for('list_sent_routes', user_id=user_id)) usei para listar as vias do usuario
+            return render_template('menu_page.html', username=username, user_id=user_id)
+        else:
+            return "Error: User not found...", 401
 
     
 
@@ -165,7 +280,8 @@ def add_route():
     #conn = sqlite3.connect('climb_API_db')
     conn = db_connection()
     cursor = conn.cursor()
-
+    
+    #
     cursor.execute('SELECT user_id FROM users WHERE user_id = %s', (user_id,))
     user = cursor.fetchone()
 
@@ -318,11 +434,9 @@ def list_sent_routes(user_id):
     conn.commit()
     conn.close()
 
-    if routes:
-        #return jsonify(routes)
-        return render_template('routes_page.html',routes=routes, username=username, crags=crags, user_id=user_id)
-    else:
-        return jsonify({'message': 'Routes not found'}), 404  
+    
+    return render_template('routes_page.html',routes=routes, username=username, crags=crags, user_id=user_id)
+
 
     
 
